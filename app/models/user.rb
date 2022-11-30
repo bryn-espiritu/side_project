bb1f40a1a3e5377a0d4d4a0cb6b7470b70a13f89
brynex20 <brynespiritu@gmail.com>
2022-11-30 19:26:15 +0800

    [Feature 1] add validate presence to username and phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_presence_of :username
+  validates_presence_of :phone
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
   mount_uploader :image, ImageUploader

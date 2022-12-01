2758caa81940c38861b4ff5ee20d6ca1b68fae35
brynex20 <brynespiritu@gmail.com>
2022-12-01 15:12:48 +0800

    [Feature 2] fix validation of devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone, :image])
   end
 end

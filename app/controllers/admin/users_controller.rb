8845a9df7bbe1eba0d106f07f8b8e3baa2862b71
brynex20 <brynespiritu@gmail.com>
2022-12-01 16:42:03 +0800

    [Feature] fix issue

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,29 +1,19 @@
 class Admin::UsersController < ApplicationController
-<<<<<<< HEAD
-<<<<<<< HEAD
-  def index
-    @users = User.all
-  end
-=======
   # before_action :authenticate_user!
   # before_action :check_admin
 
   def index
     @users = User.all
   end
-=======
+
   before_action :authenticate_user!
   before_action :check_admin
 
-  def index
-    @users = User.all
-  end
->>>>>>> 4f9f907 ([Feature 2] adding domain config | adding constraint)
 
   def check_admin
     raise ActionController::RoutingError.new("404 ERROR: Not Found") unless current_user.admin?
   end
-<<<<<<< HEAD
+
 
   before_action :set_locale
   def set_locale
@@ -33,7 +23,4 @@ class Admin::UsersController < ApplicationController
 
     I18n.locale = session[:locale] || I18n.default_locale
   end
->>>>>>> 17ab207 ([Feature 1] add set_locale to admin::user_controller)
-=======
->>>>>>> 4f9f907 ([Feature 2] adding domain config | adding constraint)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # get 'home/new'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   constraints(AdminDomainConstraint.new) do
@@ -9,33 +8,13 @@ Rails.application.routes.draw do
       devise_for :users, controllers: { sessions: 'admins/sessions' }
       resources :users
       end
-    end
+  end
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  # constraints(ClientDomainConstraint.new) do
-    resources :home
-  # end
-
-<<<<<<< HEAD
->>>>>>> 5e9c05c ([Feature 1] generate model  and added reference from user to home relation included)
-=======
-
-
->>>>>>> eb6e00e ([Feature 1] added root to home/index)
-=======
-
->>>>>>> 4f9f907 ([Feature 2] adding domain config | adding constraint)
-=======
   constraints(ClientDomainConstraint.new) do
     devise_for :users, controllers: { sessions: 'users/sessions' }
     root "home#index"
     resources :home
     resource :user_profile
   end
->>>>>>> b03ebef ([Feature 2] update routes and fix constraint)
 end
 

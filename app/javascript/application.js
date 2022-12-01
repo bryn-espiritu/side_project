3142fd66169ea9accc118dff57c906a706a3978c
brynex20 <brynespiritu@gmail.com>
2022-12-02 03:18:11 +0800

    [Feature 6] import jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import jQuery from "jquery"
+window.jQuery = jQuery
+window.$ = jQuery
+
 import * as bootstrap from "bootstrap"

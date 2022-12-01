46beebb3121436a01d4b7c66f447f71282b96cb9
brynex20 <brynespiritu@gmail.com>
2022-12-02 01:02:05 +0800

    [Feature 1] added phone validation for user phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   validates :phone, phone: { possible: true, allow_blank: true, types: [:voip, :mobile], countries: :ph }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
   mount_uploader :image, ImageUploader

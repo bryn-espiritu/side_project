2c780729ca1605e25695a49f03a4f7ccdde03713
brynex20 <brynespiritu@gmail.com>
2022-12-02 15:51:49 +0800

    [Feature 5] add location controller

---
+++ app/javascript/controllers/location_controller.js
@@ -0,0 +1,66 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ['selectedRegionId', 'selectProvinceId', 'selectCityMunicipalityId', 'selectBarangayId']
+
+    fetchProvinces() {
+        let target = this.selectProvinceIdTarget
+        $(target).empty();
+        $.ajax({
+            type: 'GET',
+            url: '/api/regions/' + this.selectedRegionIdTarget.value + '/provinces',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response)
+                $.each(response, function (index, record) {
+                    let option = document.createElement('option')
+                    option.value = record.id
+                    option.text = record.name
+                    target.appendChild(option)
+                })
+            }
+
+        })
+    }
+
+    fetchCityMunicipalities() {
+        let target = this.selectCityMunicipalityIdTarget
+        $(target).empty();
+        $.ajax({
+            type: 'GET',
+            url: '/api/regions/' + this.selectedRegionIdTarget.value + '/provinces/' + this.selectProvinceIdTarget.value + '/city_municipalities/',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response)
+                $.each(response, function (index, record) {
+                    let option = document.createElement('option')
+                    option.value = record.id
+                    option.text = record.name
+                    target.appendChild(option)
+                })
+            }
+        })
+    }
+
+    fetchBarangays() {
+        let target = this.selectBarangayIdTarget
+        $(target).empty();
+        $.ajax({
+            type: 'GET',
+            url: '/api/regions/' + this.selectedRegionIdTarget.value + '/provinces/' + this.selectProvinceIdTarget.value + '/city_municipalities/' +
+                this.selectCityMunicipalityIdTarget.value + '/barangays',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response)
+                $.each(response, function (index, record) {
+                    let option = document.createElement('option')
+                    option.value = record.id
+                    option.text = record.name
+                    target.appendChild(option)
+                })
+            }
+
+
+        })
+    }
+}

80ae6c083fbbb39efa6d7df15ac6de86d0d6181b
brynex20 <brynespiritu@gmail.com>
2022-12-02 10:31:56 +0800

    [Feature 5] fix JS issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import * as bootstrap from "bootstrap"
+
 
 import jQuery from "jquery"
 window.jQuery = jQuery
 window.$ = jQuery
 
-import * as bootstrap from "bootstrap"
+
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import LocationController from "./location_controller";
+application.register("location", LocationController)

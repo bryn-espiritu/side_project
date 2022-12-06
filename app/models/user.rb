74f23631f6b6fdb06b4bd03ab4402e015c5e2f95
brynex20 <brynespiritu@gmail.com>
2022-12-06 18:17:31 +0800

    [Feature 8 & 9] Done Partial List of Admin and increment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # ---------------------------------------------------------------------------
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
   mount_uploader :image, ImageUploader
-
   validates :phone, phone: { possible: true, allow_blank: true, types: [:voip, :mobile], countries: :ph }
-  belongs_to :parent, class_name: "User", optional: true, counter_cache: :children_members
-  has_many :children, class_name: "User", foreign_key: 'parent_id'
   enum role: { client: 0, admin: 1 }
   has_many :homes
   has_many :addresses
-
+  has_many :children, class_name: "User", foreign_key: 'parent_id'
+  belongs_to :parent, class_name: "User", optional: true, counter_cache: :children_members
 end

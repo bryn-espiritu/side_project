e4f5c596d5cec6e6d7f56281b239e0d81fcde1c7
brynex20 <brynespiritu@gmail.com>
2022-12-06 17:51:11 +0800

    [Feature] change user_controller's class abse to admin controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,26 +1,5 @@
-class Admin::UsersController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :check_admin
-
+class Admin::UsersController < AdminController
   def index
     @users = User.all
-  end
-
-  before_action :authenticate_user!
-  before_action :check_admin
-
-
-  def check_admin
-    raise ActionController::RoutingError.new("404 ERROR: Not Found") unless current_user.admin?
-  end
-
-
-  before_action :set_locale
-  def set_locale
-    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
-      session[:locale] = params[:locale]
-    end
-
-    I18n.locale = session[:locale] || I18n.default_locale
   end
 end

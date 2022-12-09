18114fa6633534f0efd79f890f63fee6529cd3d3
brynex20 <brynespiritu@gmail.com>
2022-12-09 15:14:38 +0800

    [Feature 11] add Soft Delete to Item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,25 +1,47 @@
 class Admin::ItemsController < ApplicationController
-  def index
+  before_action :set_item, only: [:edit, :update, :destroy]
 
+  def index
+    @items = Item.all
   end
 
   def new
-
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path, notice: 'Successfully Created!'
+    else
+      render :new
+    end
   end
 
-  def edit
 
-  end
+  def edit; end
 
   def update
-
+    if @item.update(item_params)
+      redirect_to admin_items_path, notice: 'Successfully Updated!'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @item.destroy
+      redirect_to admin_items_path, notice: 'Successfully Deleted!'
+    end
+  end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :minimum_bets, :state, :batch_count, :online_at, :offline_at, :start_at, :status, :quantity)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
+  has_many :item_category_ships
+  has_many :categories, through: :item_category_ships
+  # validates_presence_of :image
+  # validates_presence_of :name
+  # validates_presence_of :quantity
+  # validates_presence_of :minimum_bets
+  # validates_presence_of :state
+  # validates_presence_of :batch_count
+  # validates_presence_of :online_at
+  # validates_presence_of :offline_at
+  # validates_presence_of :start_at
+  # validates_presence_of :status
+
+  mount_uploader :image, ImageUploader
   enum status: { active: 0, inactive: 1 }
+
+  default_scope { where(deleted_at: nil) }
+  def destroy
+    update(deleted_at: Time.current)
+  end
 
 end

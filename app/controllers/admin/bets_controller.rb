a65094a2486ea89aeb79c96c1f1a36c4606ccb77
brynex20 <brynespiritu@gmail.com>
2022-12-16 01:53:40 +0800

    Add Range Date Picker at bet index

--- app/controllers/admin/bets_controller.rb
+++ app/controllers/admin/bets_controller.rb
@@ -2,10 +2,22 @@ class Admin::BetsController < AdminController
   def index
     @bets = Bet.includes(:user, :item)
     @bets = @bets.where(serial_number: params[:serial_number]) if params[:serial_number].present?
-    @bets = @bets.where(item: {name: params[:item.name]}) if params[:item.name].present?
-    @bets = @bets.where(user: {email: params[:email]}) if params[:email].present?
+    @bets = @bets.where(item: { name: params[:item.name] }) if params[:item.name].present?
+    @bets = @bets.where(user: { email: params[:email] }) if params[:email].present?
     @bets = @bets.where(state: params[:state]) if params[:state].present?
-    @bets = @bets.where()
+    @bets = @bets.where('bets.created_at >= ?', params[:created_at]) if params[:created_at].present?
+    @bets = @bets.where('bets.created_at <= ?', params[:ended_at]) if params[:ended_at].present?
+  end
+
+  def cancel
+    @bet = Bet.find(params[:bet_id])
+    if @bet.cancel!
+      flash[:notice] = "Successfully Cancelled!"
+    else
+      flash[:alert] = @bet.errors.full_messages.join(', ')
+    end
+    redirect_to admin_bets_path
   end
 end
+
 

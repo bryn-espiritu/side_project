d2d5d1668b0d9ebaf4b708f5715c0bf8ec702210
brynex20 <brynespiritu@gmail.com>
2022-12-21 18:42:32 +0800

    [Feature 21] Created Shop Page

---
+++ app/controllers/user/shop_controller.rb
@@ -0,0 +1,26 @@
+class User::ShopController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @offers = Offer.active
+    @offer = Offer.new
+  end
+
+  def create
+    @order = Order.new
+    @offer = Offer.active.find(params[:id])
+    @order.offer = @offer
+    @order.amount = @offer.amount
+    @order.coin = @offer.coin
+    @order.user = current_user
+    @order.state = :submitted
+    @order.genre = 0
+    if @order.save
+      redirect_to user_shop_index_path, notice: "Order has been Successfully Submitted "
+    else
+      render :index
+    end
+
+
+  end
+end
